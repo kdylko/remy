57260e7a60c070fbc4a38a5b63d2c294e290d701
kdylko <kdylko@gmail.com>
2018-11-22 10:43:10 +0300

    Update index.js

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -13,21 +13,21 @@ export default ({ height }) => (
 
 		<div className="description">
 			<div>
-				We believe that advances in robotics and machine learning will transform the food industry, 
-				just as they are changing many industries now. In the not-too-distant future, 
-				the meals we eat will be prepared - chopped, cooked, and served - with the help of robots.
-				This will save people - and businesses - time, lower costs, 
-				improve and standardise quality, and reduce food waste. 
+				Advances in robotics and machine learning will transform the food industry, just as 
+				they are changing many industries now. In the not-too-distant future, many of the meals we eat will 
+				be prepared - chopped, cooked, and served - with the help of robots. This will save people - 
+				and businesses - time, lower costs, improve and standardise quality, and reduce food waste. 
 			</div>
 			<div>
-				We create fully robotic platform that can cook any cuisine or any dish in the world.
-				This could be used for dark kitchens, kitchen factories, delivery-only companies, food-operators and other 
-				businesses where constant quality, adaptability to any recipe, operational effectiveness 
-				plays major role.
+				We are creating a fully robotic platform that can cook any cuisine or any dish in the world. 
+				This could be used for dark kitchens, kitchen factories, delivery-only companies, food-operators 
+				and other businesses where constant quality, adaptability to any recipe, and operational 
+				effectiveness plays major role.
 			</div>
 			<div>
-				We believe that our platform will boost development of on-demand food economy by allowing to start new 
-				food companies for a fraction of today's cost yet having the highest quality and operational efficiency.
+				We believe that our platform will boost development of the on-demand food economy by allowing 
+				food companies to operate for a fraction of today's cost, whilst still having the highest levels 
+				of quality and operational efficiency.
 			</div>
 		</div>
 	</div>
