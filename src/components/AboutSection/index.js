8e58c66332ce5510fb75596e2ec9f681ddb8714c
kdylko <kdylko@gmail.com>
2018-11-28 11:57:21 +0300

    Small corrections

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -12,13 +12,13 @@ export default ({ height }) => (
 		</div>
 
 		<div className="description">
-			<div>
+			<div className="text">
 				Advances in robotics and artificial intelligence will transform the food industry, just as 
 				they are changing many industries now. In the not-too-distant future, many of the meals we eat will 
 				be prepared - chopped, cooked, and served - with the help of robots. This will lower costs, 
 				improve and standardise quality, simplify business launch and reduce food waste. 
 			</div>
-			<div>
+			<div className="text">
 				We are creating food robotics solutions that could be used for dark kitchens, 
 				kitchen factories, restaurants and other businesses where constant quality, adaptability to any 
 				recipe, and operational effectiveness plays major role.
